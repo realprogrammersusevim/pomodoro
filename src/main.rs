9c09474d1e4afef06fb30d6d20ef065091dcd09e
Jonathan Milligan <logouts.deco-0e@icloud.com>
2022-12-15 13:41:34 -0600

    feat: Name work sessions
    I added the ability to name work sessions. Use the `-n` option to pass a
    string to be displayed at the top of the work timer.

--- src/main.rs
+++ src/main.rs
@@ -15,26 +15,34 @@ struct Args {
     work: u64,
 
     // How long the break time should be
-    #[arg(short, long, help = "Chill duration")]
+    #[arg(short, long, help = "Chill duration", default_value = "0")]
     chill: u64,
 
     // How many times to repeat the cycle
     #[arg(short, long, default_value = "1", help = "How many times to repeat")]
     repeat: u64,
+
+    // Optionally name the work session
+    #[arg(short, long, help = "Name of the work session")]
+    name: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
     for _ in 0..args.repeat {
-        println!("Work time for {} minutes!", args.work);
         let work_pb = ProgressBar::new(args.work * 60);
+
+        if let Some(name) = &args.name {
+            println!("Work session: {}", name);
+        } else {
+            println!("Work session");
+        }
         for _ in 0..args.work * 60 {
             work_pb.inc(1);
             thread::sleep(time::Duration::from_secs(1));
         }
 
-        println!("Chill time for {} minutes!", args.chill);
         let chill_pb = ProgressBar::new(args.chill * 60);
         for _ in 0..args.chill * 60 {
             chill_pb.inc(1);
